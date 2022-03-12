cb866eab9612c26c996473946613277d88d82581
Michel Thomas Brodatzki <michel@brodatzki.com>
2022-03-12 17:16:48 +0100

    Setup basic python project

---
+++ app.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+
+def main():
+    """Entrypoint for pdf-feedback app."""
+    # Loading .env file. See .env.template for more information.
+    load_dotenv()
+
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
