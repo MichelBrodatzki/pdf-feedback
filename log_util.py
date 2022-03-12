7aa46a464d709a8767adf63cee8ab2f4c11782f7
Michel Thomas Brodatzki <michel@brodatzki.com>
2022-03-12 19:09:01 +0100

    Remove redundant logging util

--- log_util.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-def log(message):
-    print ("[{}] {}".format(datetime.now(), message))
-
-def error(message):
-    # TODO (Brodi): Implement better error logging
-    log(message)
